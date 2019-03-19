487977ce4374d9fea7dfe549fb656e54cd902a77
Sofia Patrocinio <sofiapatrocinio@hotmail.com>
2019-03-19 08:35:44 -0300

    Adding messages

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,19 @@
-# -*- coding: utf-8 -*-
-from chatterbot.trainers import ListTrainer
-from chatterbot import ChatBot
+#-*- coding: utf-8 -*-
 
-bot = ChatBot('Test') 
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+import os
+
+bot = ChatBot('Teste')
+
+trainer = ListTrainer(bot)
+
+for arq in os.listdir('arq'):
+    chats = open('arq/' + arq, 'r').readlines()
+    trainer.train(chats)
+
+while True:
+    resq = input('Você: ')
+
+    resp = bot.get_response(resq)
+    print ('Bot: ' + str(resp))
